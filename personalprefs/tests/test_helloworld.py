7289addc57d139924f0797cc1b25148ed1626ea3
Jeff Franklin <jpf@uw.edu>
2015-02-11 10:22:06 -0800

    Get an initial test server in for the build server. Everything I'm adding can go away. (EDS-98)

---
+++ personalprefs/tests/test_helloworld.py
@@ -0,0 +1,5 @@
+from personalprefs.hello_world import hello_world
+
+
+def test_hello_world():
+    assert hello_world() == 'hello world!'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+
+install_requires = []
+# eg install_requires = ['urllib3', 'lxml', 'pytest', 'mock', 'tox', 'pyyaml']
+
+
+setup(name='personalpreferences',
+      version='0.1',
+      description='personalpreferences',
+      install_requires=install_requires,
+      )
