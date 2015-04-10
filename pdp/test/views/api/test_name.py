ac1c022fa3421e0bf897220b90138ef0e8315966
Jeff Franklin <jpf@uw.edu>
2015-04-09 18:43:39 -0700

    Catch invalid put data and return 400 (EDS-190)

--- pdp/test/views/api/test_name.py
+++ pdp/test/views/api/test_name.py
@@ -2,39 +2,75 @@ from pdp.views.api.name import Name
 import mock
 import logging
 import json
+from restclients.exceptions import InvalidIRWSName, DataFailureException
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-@mock.patch('pdp.views.api.name.IRWS')
-def test_name_get(irws):
-    name = Name()
-    request = mock.MagicMock()
-    request.user.username = 'jjj@washington.edu'
-    irws.return_value.get_name_by_netid.return_value.json_data.return_value = {
-        'foo': 'bar'}
+class TestName(object):
 
-    response = name.GET(request)
+    def setup_method(self, method):
+        logger.debug('setting up method {}'.format(method.__name__))
+        self.patchers = {'irws': mock.patch('pdp.views.api.name.IRWS')}
 
-    irws.return_value.get_name_by_netid.assert_called_once_with('jjj')
-    assert response.status_code == 200
-    response_name = json.loads(response.content)
-    assert response_name['foo'] == 'bar'
+        for attr, patcher in self.patchers.iteritems():
+            setattr(self, attr, patcher.start())
 
+    def teardown_method(self, method):
+        for patcher in self.patchers.values():
+            patcher.stop()
 
-@mock.patch('pdp.views.api.name.IRWS')
-def test_name_put(irws):
-    name = Name()
-    request = mock.MagicMock()
-    request.user.username = 'jjj@washington.edu'
-    request.body = json.dumps({'foo': 'bar'})
-    irws.return_value.put_name_by_netid.return_value = {
-        'baz': 'bar'}
+    def test_name_get(self):
+        name = Name()
+        request = self._mock_request()
+        get_name = self.irws.return_value.get_name_by_netid
+        get_name.return_value.json_data.return_value = {
+            'foo': 'bar'}
 
-    response = name.PUT(request)
+        response = name.GET(request)
 
-    irws.return_value.put_name_by_netid.assert_called_once_with(
-        'jjj', request.body)
-    assert response.status_code == 200
-    assert json.loads(response.content)['baz'] == 'bar'
+        get_name.assert_called_once_with('jjj')
+        assert response.status_code == 200
+        response_name = json.loads(response.content)
+        assert response_name['foo'] == 'bar'
+
+    def test_name_put(self):
+        name = Name()
+        request = self._mock_request({'foo': 'bar'})
+
+        self.irws.return_value.put_name_by_netid.return_value = {
+            'baz': 'bar'}
+
+        response = name.PUT(request)
+
+        self.irws.return_value.put_name_by_netid.assert_called_once_with(
+            'jjj', request.body)
+        assert response.status_code == 200
+        assert json.loads(response.content)['baz'] == 'bar'
+
+    def test_name_put_bad_name(self):
+        name = Name()
+        request = self._mock_request({})
+        put_name = self.irws.return_value.put_name_by_netid
+        put_name.side_effect = InvalidIRWSName('bad name')
+
+        response = name.PUT(request)
+        assert response.status_code == 400
+
+    def test_name_put_data_exception(self):
+        dfe = DataFailureException('j', 300, '')
+        self.irws.return_value.put_name_by_netid.side_effect = dfe
+        exception_caught = False
+        try:
+            Name().PUT(self._mock_request({}))
+        except:
+            exception_caught = True
+        assert exception_caught
+
+    def _mock_request(self, body=None):
+        request = mock.MagicMock()
+        request.user.username = 'jjj@washington.edu'
+        if body is not None:
+            request.body = json.dumps(body)
+        return request
--- pdp/views/api/name.py
+++ pdp/views/api/name.py
@@ -1,9 +1,10 @@
 import logging
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 import json
 import re
 
 from restclients.irws import IRWS
+from restclients.exceptions import DataFailureException
 
 from pdp.views.rest_dispatch import RESTDispatch, data_not_found
 
@@ -26,7 +27,14 @@ class Name(RESTDispatch):
         logger.debug('name api put')
         remote_user = re.sub(r'@.*', '', request.user.username)
 
-        irws = IRWS()
-        pn = irws.put_name_by_netid(remote_user, request.body)
-        return HttpResponse(json.dumps(pn),
-                            content_type='application/json')
+        try:
+            pn = IRWS().put_name_by_netid(remote_user, request.body)
+            response = HttpResponse(json.dumps(pn),
+                                    content_type='application/json')
+        except DataFailureException as dfe:
+            logger.info(str(dfe))
+            raise dfe
+        except Exception as e:
+            logger.info('exception {} occurred'.format(type(e).__name__))
+            response = HttpResponseBadRequest()
+        return response
