19a425ed37815b45d8ba879b62cc2da2d05d6e8f
Jeff Franklin <jpf@uw.edu>
2015-05-08 10:31:09 -0700

    Turn back on % as a valid character now that IRWS doesn't urldecode json payloads as of 4.1.2. EDS-194

--- pdp/static/js/pdp-app.js
+++ pdp/static/js/pdp-app.js
@@ -32,7 +32,7 @@ app.filter('invalid_chars', function() {
 app.controller('NameCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
 
     // sample valid name characters
-    $scope.valid_chars = /^[\w !#$&\'*+\-,.?^_`{}~]*$/;
+    $scope.valid_chars = /^[\w !#$%&\'*+\-,.?^_`{}~]*$/;
     $scope.displayNameMax = 80;
 
     // display names as they look in the directory
