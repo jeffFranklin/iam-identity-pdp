8d33ddfc62d0f009644f6776dbace63855209347
Jeff Franklin <jpf@uw.edu>
2015-05-22 17:05:21 -0700

    Enable publish flag in our internal api. Still needs front-end enabling. EDS-110

--- pdp/views/api/identity.py
+++ pdp/views/api/identity.py
@@ -1,47 +1,69 @@
 # identity apis
 
 import logging
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseBadRequest
 import json
-import re
-
+from restclients.exceptions import DataFailureException, IRWSPersonNotFound
 from restclients.irws import IRWS
+from pdp.views.rest_dispatch import RESTDispatch
+from pdp.util import Util
 
-from pdp.views.rest_dispatch import RESTDispatch, data_not_found
-
+logger = logging.getLogger(__name__)
 
 # get/set publish flag
+
 
 class Publish(RESTDispatch):
 
     def GET(self, request):
-
-        logger = logging.getLogger('pdp')
-        logger.info("identity/publish api")
-        remote_user = re.sub(r'@.*', '', request.user.username)
-        irws = IRWS()
-        ident = irws.get_identity_by_netid(remote_user)
-        wp_publish = 'true'
-        if 'hepps' in ident.identifiers:
-            uri = ident.identifiers['hepps']
-            hepps = irws.get_hepps_person_by_uri(uri)
-            if hepps.wp_publish != 'Y':
-                wp_publish = 'false'
-        resp = {}
-        resp['wp_publish'] = wp_publish
-        return HttpResponse(json.dumps(resp),
-                            content_type='application/json')
+        logger.info("identity/publish api for " + request.user.username)
+        netid = Util.netid_from_remote_user(request.user.username)
+        try:
+            person = IRWS().get_hepps_person_by_netid(netid)
+            response = HttpResponse(
+                self._person_object_to_json(person),
+                content_type='application/json')
+        except IRWSPersonNotFound as ipnf:
+            response = HttpResponseNotFound()
+        except DataFailureException as dfe:
+            logger.info(str(dfe))
+            raise dfe
+        return response
 
     def PUT(self, request):
-
-        logger = logging.getLogger('pdp')
-        logger.debug('identity/publish api put')
-        remote_user = re.sub(r'@.*', '', request.user.username)
+        logger.info('identity/publish api put for' + request.user.username)
+        netid = Util.netid_from_remote_user(request.user.username)
 
         irws = IRWS()
+        try:
+            # success or exception
+            irws.post_hepps_person_by_netid(
+                netid,
+                self._json_to_irws_json(request.body))
+            response = HttpResponse(json.dumps({'message': 'successful put'}),
+                                    content_type='application_json',
+                                    status=200)
+        except DataFailureException as dfe:
+            logger.info(str(dfe))
+            raise dfe
+        except IRWSPersonNotFound as ipnf:
+            logger.info('attempted to post for non-hepps person ' + netid)
+            response = HttpResponseNotFound()
+        except Exception as e:
+            logger.info('exception {} occurred: {}'.format(
+                type(e).__name__, str(e)))
+            response = HttpResponseBadRequest()
 
-        # pn = irws.put_publish_by_netid(remote_user, request.body)
-        errtxt = '{"message": "Save is not yet implemented."}'
-        return HttpResponse(json.dumps(errtxt),
-                            content_type='application/json',
-                            status=501)
+        return response
+
+    def _person_object_to_json(self, person):
+        return json.dumps({'publish': person.wp_publish})
+
+    def _json_to_irws_json(self, data):
+        try:
+            person = json.loads(data)
+            irws_json = json.dumps({'wp_publish': person['publish']})
+        except:
+            raise ValueError('bad json from browser')
+        return irws_json
--- pdp/views/api/name.py
+++ pdp/views/api/name.py
@@ -1,34 +1,34 @@
 import logging
 from django.http import HttpResponse, HttpResponseBadRequest
 import json
-import re
 
 from restclients.irws import IRWS
 from restclients.exceptions import DataFailureException
 
-from pdp.views.rest_dispatch import RESTDispatch, data_not_found
+from pdp.views.rest_dispatch import RESTDispatch
+from pdp.util import Util
+
+logger = logging.getLogger(__name__)
 
 
 class Name(RESTDispatch):
 
     def GET(self, request):
 
-        logger = logging.getLogger('pdp')
         logger.info("name api")
-        remote_user = re.sub(r'@.*', '', request.user.username)
+        netid = Util.netid_from_remote_user(request.user.username)
         irws = IRWS()
-        name = irws.get_name_by_netid(remote_user)
+        name = irws.get_name_by_netid(netid)
         return HttpResponse(json.dumps(name.json_data()),
                             content_type='application/json')
 
     def PUT(self, request):
 
-        logger = logging.getLogger('pdp')
         logger.debug('name api put')
-        remote_user = re.sub(r'@.*', '', request.user.username)
+        netid = Util.netid_from_remote_user(request.user.username)
 
         try:
-            pn = IRWS().put_name_by_netid(remote_user, request.body)
+            pn = IRWS().put_name_by_netid(netid, request.body)
             response = HttpResponse(json.dumps(pn),
                                     content_type='application/json')
         except DataFailureException as dfe:
