4a284fc09a585753479faad46327571fa2cffa40
Jeff Franklin <jpf@washington.edu>
2016-03-01 11:05:52 -0800

    Minor tweaks to settings.

--- pdp-site/docker_settings.py
+++ pdp-site/docker_settings.py
@@ -5,3 +5,4 @@ RESTCLIENTS_IRWS_DAO_CLASS = 'restclients.dao_implementation.irws.File'
 RESTCLIENTS_IRWS_SERVICE_NAME = 'registry-dev'
 LOGIN_URL = '/id/mocklogin'  # Hide the LOGIN_URL from shib
 MIDDLEWARE_CLASSES.insert(0, 'idbase.middleware.MockLoginMiddleware')
+MOCK_LOGIN_USER = 'user1e@washington.edu'
--- pdp-site/it_settings.py
+++ pdp-site/it_settings.py
@@ -1,6 +1,5 @@
 from settings import *
 
-MIDDLEWARE_CLASSES = tuple(x for x in MIDDLEWARE_CLASSES
-                           if x != 'django.middleware.csrf.CsrfViewMiddleware')
-
+MIDDLEWARE_CLASSES = [x for x in MIDDLEWARE_CLASSES
+                      if x != 'django.middleware.csrf.CsrfViewMiddleware']
 LOGGING = {}
