c55583f0dfd98e600ce7c5536cb10d5a43c58b94
Jeff Franklin <jpf@uw.edu>
2016-05-31 16:54:23 -0700

    Let all but non-persons in and give a more friendly error. EDS-234

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,55 +1,10 @@
-from pdp.models import BaseModel, Profile
-from pytest import mark, raises
+from pdp.models import Profile
 try:  # http://asq.googlecode.com/hg-history/1.0/asq/_portability.py
     # Python 2
     from itertools import izip_longest
 except ImportError:
     # Python 3
     from itertools import zip_longest as izip_longest
-
-
-class FooModel(BaseModel):
-    netid = None
-    sub_foo = None
-    _specials = {'sub_foo': 'test_models.FooSubModel'}
-
-
-class FooSubModel(BaseModel):
-    netid = None
-
-
-def test_base_model_no_dict():
-    model = FooModel()
-    assert model.netid is None
-    assert model.sub_foo is None
-
-
-def test_base_model_good_dict():
-    model = FooModel(netid='joe', sub_foo=dict(netid='blow'))
-    assert model.netid == 'joe'
-    assert model.sub_foo.netid == 'blow'
-
-
-def test_base_model_to_dict():
-    model = FooModel(netid='joe', sub_foo=dict(netid='blow'))
-    assert model.to_dict() == {'netid': 'joe', 'sub_foo': {'netid': 'blow'}}
-
-
-def test_base_model_to_dict_no_sub():
-    model = FooModel(netid='joe')
-    assert model.to_dict() == {'netid': 'joe', 'sub_foo': None}
-
-
-@mark.parametrize('dct', [
-    {'netid': 'joe', 'name': 'bad'},
-    {'netid': 'joe', 'sub_foo': {'bad': 'bad'}},
-    {'netid': 'joe', '_hidden': 'bad'},
-    {'netid': 'joe', 'sub_foo': {'_hidden': 'bad'}}],
-    ids=['bad attribute', 'bad sub attribute', 'underscore attibute',
-         'underscore sub attibute'])
-def test_base_model_bad_dict(dct):
-    with raises(ValueError):
-        FooModel(**dct)
 
 
 def test_profile():
